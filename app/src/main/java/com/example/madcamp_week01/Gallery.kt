c714296bad1e48f5947cbeb4ebfe74dddb1a1882
Seunghyo <shyo0218@naver.com>
2023-12-29 12:40:39 +0900

    gallery 크기 수정 및 레이아웃 추가

--- app/src/main/java/com/example/madcamp_week01/Gallery.kt
+++ app/src/main/java/com/example/madcamp_week01/Gallery.kt
@@ -1,10 +1,19 @@
 package com.example.madcamp_week01
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -27,7 +36,66 @@ class Gallery : Fragment() {
         val adapter = GalleryAdapter(imageList)
         recyclerView.adapter = adapter
 
+        val imageCountTextView: TextView = view.findViewById(R.id.imageCountTextView)
+        imageCountTextView.text = "총 ${imageList.size} 개의 이미지"
+
         return view
+    }
+
+    private fun pickImageFromGallery() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                openGallery()
+            } else {
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+        } else {
+            openGallery()
+        }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Permission denied. You can't access images.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val selectedImage: Uri? = data.data
+            if (selectedImage != null) {
+                // 이미지가 선택된 경우, 리스트에 추가하고 어댑터에 알림
+                imageList.add(selectedImage.toString())
+                adapter.notifyDataSetChanged()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/madcamp_week01/GalleryAdapter.kt
+++ app/src/main/java/com/example/madcamp_week01/GalleryAdapter.kt
@@ -2,6 +2,8 @@ package com.example.madcamp_week01
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,11 +20,13 @@ class GalleryAdapter(private val imageList: List<Int>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        // viewType 형태 아이템 뷰를 위한 뷰홀더 객체를 생성
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
         return ImageViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        // position에 해당하는 데이터를 뷰홀더 아이템뷰에 표시
         val imageUrl = imageList[position]
 
         Glide.with(holder.itemView.context)
@@ -35,6 +39,7 @@ class GalleryAdapter(private val imageList: List<Int>) :
     }
 
     override fun getItemCount(): Int {
+        //전체 아이템 개수 리턴
         return imageList.size
     }
 
@@ -42,6 +47,9 @@ class GalleryAdapter(private val imageList: List<Int>) :
         val dialog = Dialog(context)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(R.layout.popup_image)
+
+        val window = dialog.window
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         val popupImageView: ImageView = dialog.findViewById(R.id.popupImageView)
         Glide.with(context)
