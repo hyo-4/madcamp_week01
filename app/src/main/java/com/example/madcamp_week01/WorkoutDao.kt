454fbb0f26a9d69cc47a28f3a2ef80dddd66bd73
chanee718 <chanee718@kaist.ac.kr>
2023-12-30 22:33:56 +0900

    breakfast transaction updated

--- app/src/main/java/com/example/madcamp_week01/WorkoutDao.kt
+++ app/src/main/java/com/example/madcamp_week01/WorkoutDao.kt
@@ -9,6 +9,8 @@ import androidx.room.Query
 
 @Dao
 interface WorkoutDao {
+    @Query("SELECT * FROM tb_workout WHERE year = :year AND month = :month AND date = :date")
+    fun getWorkoutByDate(year: Int, month: Int, date: Int): List<Workout>?
 
     @Query("SELECT * FROM tb_workout ORDER BY year, month, date ASC")
     fun getAll(): List<Workout>
