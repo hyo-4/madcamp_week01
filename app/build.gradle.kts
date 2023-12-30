0aacf1301888c8e9a5f259ad2561bdc17216b6b6
Seunghyo <shyo0218@naver.com>
2023-12-30 11:19:54 +0900

    gradle setting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,15 +49,27 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
-    implementation("androidx.room:room-runtime:2.2.5")
-    kapt("androidx.room:room-compiler:2.2.5")
-    implementation("androidx.room:room-ktx:2.2.5")
-    implementation("androidx.room:room-rxjava2:2.2.5")
-    implementation("androidx.room:room-guava:2.2.5")
-    testImplementation("androidx.room:room-testing:2.2.5")
+    implementation("androidx.room:room-runtime:2.2.6")
+    kapt("androidx.room:room-compiler:2.2.6")
+    implementation("androidx.room:room-ktx:2.2.6")
+    implementation("androidx.room:room-rxjava2:2.2.6")
+    implementation("androidx.room:room-guava:2.2.6")
+    kapt("org.xerial:sqlite-jdbc:3.34.0")
+    androidTestImplementation("androidx.room:room-testing:2.2.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+    kapt ("androidx.lifecycle:lifecycle-compiler:2.2.0")
+    // UI
+    implementation ("com.google.android.material:material:1.1.0")
+    // Testng
+    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.11.0")
 }
+
