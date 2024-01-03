c655e6784d42069982cb8cc22c91bad03c60e0e0
Seunghyo <shyo0218@naver.com>
2024-01-03 13:26:03 +0900

    change

--- app/src/main/java/com/example/madcamp_week01/NewGalleryAdapter.kt
+++ app/src/main/java/com/example/madcamp_week01/NewGalleryAdapter.kt
@@ -12,6 +12,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 
 class NewGalleryAdapter(var WorkoutList: List<Workout>?) : RecyclerView.Adapter<NewGalleryAdapter.ContactViewHolder>() {
 
@@ -33,6 +37,10 @@ class NewGalleryAdapter(var WorkoutList: List<Workout>?) : RecyclerView.Adapter<
         fun bind(workout: Workout, position: Int) {
             index = position
             Log.d("workout",workout.toString())
+            val transformation = MultiTransformation(
+                CenterCrop(),
+                RoundedCorners(50)
+            )
 
             workout.workoutImg?.let {
                 Glide.with(itemView.context).load(it).into(workoutImageView)
@@ -52,7 +60,10 @@ class NewGalleryAdapter(var WorkoutList: List<Workout>?) : RecyclerView.Adapter<
 
             // Load and display breakfast image if it exists
             workout.breakfastImg?.let {
-                Glide.with(itemView.context).load(it).into(breakfastImageView)
+                Glide.with(itemView.context)
+                    .load(it)
+                    .apply(RequestOptions.bitmapTransform(transformation))
+                    .into(breakfastImageView)
                 breakfastImageView.visibility = View.VISIBLE
                 breakfastView.visibility = View.VISIBLE
                 breakfastImageView.setOnClickListener {
